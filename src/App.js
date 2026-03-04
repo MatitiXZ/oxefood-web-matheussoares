08b6bf80719623de256f681cc201f85dfd9c9931
Matheus Soares <50336241+MatitiXZ@users.noreply.github.com>
2026-03-04 02:01:02 -0300

    código de backend inicial de entregador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
-import FormProduto from './views/produto/FormProduto';
+import FormEntregador from './views/entregador/FormEntregador';
 
 function App() {
   return (
     <div className="App">
-      <FormProduto />
+      <FormEntregador />
 
       <div style={{marginTop: '6%'}}>
         <Segment vertical color='grey' size='tiny' textAlign='center'>
--- src/views/entregador/FormEntregador.jsx
+++ src/views/entregador/FormEntregador.jsx
@@ -1,25 +1,71 @@
+import axios from "axios";
 import InputMask from "comigo-tech-react-input-mask";
-import React from "react";
+import { useState } from "react";
 import {
   Button,
   Container,
   Divider,
   Form,
-  FormGroup,
-  FormInput,
-  FormRadio,
   FormField,
-  Icon,
-  TextArea,
+  FormInput,
+  Icon
 } from "semantic-ui-react";
 
 const opcoesUF = [
-  { text: "PE", value: "pernambuco" },
-  { text: "SP", value: "sao paulo" },
-  { text: "RJ", value: "rio de janeiro" },
+  { text: "PE", value: "PE" },
+  { text: "SP", value: "SP" },
+  { text: "RJ", value: "RJ" },
 ];
 
 export default function FormProduto() {
+
+  const [nome, setNome] = useState();
+  const [cpf, setCpf] = useState();
+  const [rg, setRg] = useState();
+  const [dataNascimento, setDataNascimento] = useState();
+  const [foneCelular, setFoneCelular] = useState();
+  const [foneFixo, setFoneFixo] = useState();
+  const [qtdEntregasRealizadas, setQtdEntregasRealizadas] = useState();
+  const [valorFrete, setValorFrete] = useState();
+  const [enderecoRua, setEnderecoRua] = useState();
+  const [enderecoComplemento, setEnderecoComplemento] = useState();
+  const [enderecoNumero, setEnderecoNumero] = useState();
+  const [enderecoBairro, setEnderecoBairro] = useState();
+  const [enderecoCidade, setEnderecoCidade] = useState();
+  const [enderecoCep, setEnderecoCep] = useState();
+  const [enderecoUf, setEnderecoUf] = useState();
+  const [ativo, setAtivo] = useState();
+
+  function salvar() {
+
+		let EntregadorRequest = {
+		     nome: nome,
+		     cpf: cpf,
+         rg: rg,
+		     dataNascimento: dataNascimento,
+		     foneCelular: foneCelular,
+		     foneFixo: foneFixo,
+         qtdEntregasRealizadas: qtdEntregasRealizadas,
+         valorFrete: valorFrete,
+         enderecoRua: enderecoRua,
+         enderecoComplemento: enderecoComplemento,
+         enderecoNumero: enderecoNumero,
+         enderecoBairro: enderecoBairro,
+         enderecoCidade: enderecoCidade,
+         enderecoCep: enderecoCep,
+         enderecoUf: enderecoUf,
+         ativo: ativo
+		}
+	
+		axios.post("http://localhost:8080/api/entregador", EntregadorRequest)
+		.then((response) => {
+		     console.log('Entregador cadastrado com sucesso.')
+		})
+		.catch((error) => {
+		     console.log('Erro ao incluir o um entregador.')
+		})
+	}
+
   return (
     <div>
       <div style={{ marginTop: "3%" }}>
@@ -46,14 +92,24 @@ export default function FormProduto() {
                   maxLength="100"
                   width={10}
                 >
-                  <InputMask placeholder="" />
+                  <InputMask 
+                  placeholder="" 
+                  value={nome}
+                  onChange={e => setNome(e.target.value)}
+                  />
                 </Form.Input>
 
                 <Form.Input required fluid label="CPF" width={5}>
-                  <InputMask required mask="999.999.999-99" />
+                  <InputMask required mask="999.999.999-99" 
+                  value={cpf}
+                  onChange={e => setCpf(e.target.value)}
+                  />
                 </Form.Input>
 
-                <FormInput label="RG" width={5}></FormInput>
+                <FormInput label="RG" width={5}
+                value={rg}
+                onChange={e => setRg(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group>
@@ -62,37 +118,66 @@ export default function FormProduto() {
                     mask="99/99/9999"
                     maskChar={null}
                     placeholder="Ex:20/03/1985"
+                    value={dataNascimento}
+                    onChange={e => setDataNascimento(e.target.value)}
                   />
                 </FormInput>
 
                 <Form.Input required fluid label="Fone Celular" width={5}>
-                  <InputMask mask="(99) 9999.9999" />
+                  <InputMask 
+                  mask="(99) 9999.9999" 
+                  value={foneCelular}
+                  onChange={e => setFoneCelular(e.target.value)}
+                  />
                 </Form.Input>
 
                 <Form.Input fluid label="Fone Fixo" width={5}>
-                  <InputMask mask="(99) 9999.9999" />
+                  <InputMask mask="(99) 9999.9999" 
+                  value={foneFixo}
+                  onChange={e => setFoneFixo(e.target.value)}
+                  />
                 </Form.Input>
 
                 <Form.Input
                   label="QTD Entregas Realizadas"
                   width={5}
+                  value={qtdEntregasRealizadas}
+                  onChange={e => setQtdEntregasRealizadas(e.target.value)}
                 ></Form.Input>
 
-                <Form.Input label="Valor Por Frete" width={5}></Form.Input>
+                <Form.Input label="Valor Por Frete" width={5}
+                  value={valorFrete}
+                  onChange={e => setValorFrete(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group>
-                <Form.Input label="Rua" width={14}></Form.Input>
+                <Form.Input label="Rua" width={14}
+                  value={enderecoRua}
+                  onChange={e => setEnderecoRua(e.target.value)}
+                />
 
-                <Form.Input label="Número" width={6}></Form.Input>
+                <Form.Input label="Número" width={6}
+                  value={enderecoNumero}
+                  onChange={e => setEnderecoNumero(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group>
-                <Form.Input label="Bairro" width={8}></Form.Input>
+                <Form.Input label="Bairro" width={8}
+                  value={enderecoBairro}
+                  onChange={e => setEnderecoBairro(e.target.value)}
+                />
 
-                <Form.Input label="Cidade" width={8}></Form.Input>
+                <Form.Input label="Cidade" width={8}
+                  value={enderecoCidade}
+                  onChange={e => setEnderecoCidade(e.target.value)}
+                />
 
-                <Form.Input label="CEP" width={4}></Form.Input>
+                <Form.Input label="CEP" width={4}
+                  value={enderecoCep}
+                  onChange={e => setEnderecoCep(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Select
@@ -100,17 +185,24 @@ export default function FormProduto() {
                 label="UF"
                 options={opcoesUF}
                 placeholder="Selecione"
+                value={enderecoUf}
+                onChange={(e, { value }) => setEnderecoUf(value)}
               />
 
-              <Form.Input label="Complemento"></Form.Input>
+              <Form.Input label="Complemento"
+                value={enderecoComplemento}
+                onChange={e => setEnderecoComplemento(e.target.value)}
+              />
 
               <Form.Group inline>
-                <label>Ativo</label>
+                <label>Ativo:</label>
                 <FormField
                   label="Sim"
                   control="input"
                   type="radio"
                   name="htmlRadios"
+                  value={true}
+                  onChange={e => setAtivo(e.target.value)}
                 />
 
                 <FormField
@@ -118,6 +210,8 @@ export default function FormProduto() {
                   control="input"
                   type="radio"
                   name="htmlRadios"
+                  value={false}
+                  onChange={e => setEnderecoComplemento(e.target.value)}
                 />
               </Form.Group>
             </Form>
@@ -142,6 +236,7 @@ export default function FormProduto() {
                 labelPosition="left"
                 color="blue"
                 floated="right"
+                onClick={() => salvar()}
               >
                 <Icon name="save" />
                 Salvar
